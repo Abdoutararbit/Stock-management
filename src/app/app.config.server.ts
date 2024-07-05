65e2673f7ddf8c5f0299034ca3a470eb862df6b1
abdo <abdenourtararbit1998@gmail.com>
2024-07-05 22:51:58 +0100

    Initial commit

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,21 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
+// app.module.ts (ou login.module.ts si vous utilisez un module spécifique pour login)
 
-const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
-};
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Importez FormsModule
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { routes } from './app.routes'; // Importez vos routes ici
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    FormsModule, // Ajoutez FormsModule ici
+    RouterModule.forRoot(routes), // Configurez les routes ici
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
